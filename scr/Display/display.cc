27801178a6d4359ad7a0dd6092e80eb1c0b576af
Dhust_Kode <rafaelmoura.dev@gmail.com>
2015-08-12 21:30:29 +0000

    up

--- scr/Display/display.cc
+++ scr/Display/display.cc
@@ -19,9 +19,21 @@ SysDisplay::SysDisplay(uint32_t _width, uint32_t _height, double _frames_per_sec
 	win = SDL_CreateWindow("SysDisplay",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,
 				width,height,SDL_WINDOW_RESIZABLE);
 	
-
+	win_surface = SDL_GetWindowSurface(win);
 	
 }
+
+
+
+void SysDisplay::ClearScreen() const
+{
+	std::fill_n(static_cast<uint32_t*>(win_surface->pixels),win_max_size,~0);
+}
+
+
+
+
+
 
 void SysDisplay::UpdateWindow() const
 {
@@ -34,6 +46,7 @@ SysDisplay::~SysDisplay()
 {
 
 	std::cout << "Destroying SysDisplay\n";	
+	SDL_FreeSurface(win_surface);
 	SDL_DestroyWindow(win);
 
 }
@@ -43,11 +56,9 @@ SysDisplay::~SysDisplay()
 int main()
 {
 	SysDisplay disp(800,600,60);
-//	disp.ClearScreen();
-	while(1)
-	{
-		disp.UpdateWindow();
-	}
+	
+	disp.UpdateWindow();
+	SDL_Delay(1000*4);
 
 
 
--- scr/Display/display.hh
+++ scr/Display/display.hh
@@ -3,10 +3,8 @@ class SysDisplay
 {
 public:
 	SysDisplay(uint32_t _width, uint32_t _height, double _frames_per_second);
-	
-	
+	void ClearScreen() const;
 	virtual void UpdateWindow() const;
-	
 	virtual ~SysDisplay();
 
 protected:
@@ -14,7 +12,9 @@ protected:
 	uint32_t height;
 	double frames_per_sec;
 	size_t win_max_size;
+private:
 	SDL_Window *win;
+	SDL_Surface *win_surface;
 
 	
 
